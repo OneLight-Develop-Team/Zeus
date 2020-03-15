025ded0d4d1eac93f04c26f9aba7e7ed545e8586
PeiXinHuang <1716973911@qq.com>
2020-03-15 21:21:54 +0800

    PIL

--- Windows/Tag.py
+++ Windows/Tag.py
@@ -3,11 +3,11 @@
 from PySide2.QtWidgets import QWidget, QPushButton, QApplication, QHBoxLayout,QLineEdit
 from PySide2.QtUiTools import QUiLoader
 from PySide2.QtCore import Qt
-
-from Zeus.Windows.layouitflow import FlowLayout
+from Zeus.settings import GenImage
+from Zeus.Windows import layouitflow
 import os,json
-
-
+reload(layouitflow)
+reload(GenImage)
 #获取当前文件所在路径
 current_path = os.path.dirname(os.path.abspath(__file__))
 
@@ -15,14 +15,15 @@ file_path = os.path.dirname(current_path)
 
 
 
-class TabWidget(QWidget):
+class TagWidget(QWidget):
     """
         标签选择窗口类
     参数：
         文件路径
     """
+    
     def __init__(self,paths):
-        super(TabWidget, self).__init__()
+        super(TagWidget, self).__init__()
 
         self.paths = paths  #文件路径列表
 
@@ -60,7 +61,7 @@ class TabWidget(QWidget):
     # Todo: 将读取标签数据，改到从数据库读取
     # 从json文件里的Tags键读取已经有的标签,设置窗口的标签
     def setTags(self):
-        self.widget.setLayout(FlowLayout())
+        self.widget.setLayout(layouitflow.FlowLayout())
         
         #加载标签数据到字典
         with open(file_path + r"\res\temp\setting.json") as js:
@@ -71,7 +72,7 @@ class TabWidget(QWidget):
         
         # 根据标签数据添加标签按钮
         for tag in setting_json["Tags"]:
-            self.widget.layout().addWidget(TabBtn(tag))
+            self.widget.layout().addWidget(TagBtn(tag))
 
 
         
@@ -124,7 +125,15 @@ class TabWidget(QWidget):
             asset_json[path]["path"] = path  #保存地址
             asset_json[path]["name"] = (path.split("/"))[-1]   #保存文件名
             asset_json[path]["type"] = os.path.splitext(path)[-1]  #保存文件类型，后缀名
-            asset_json[path]["tags"] = tags #保存文件标签
+            asset_json[path]["tags"] = tags  #保存文件标签
+            
+
+            # 如果是图片数据，则生成缩略图，保存缩略图地址到json文件
+            if asset_json[path]["type"] == ".jpg" or asset_json[path]["type"] == ".jpeg" or asset_json[path]["type"] == ".png":
+                thumbnail_path = GenImage.make_thumb(path)
+                asset_json[path]["thumbnail"] =  thumbnail_path
+
+
 
         #写入标签数据到json文件中
         with open(file_path + r"\res\temp\asset.json", 'w') as json_file:
@@ -153,10 +162,28 @@ class TabWidget(QWidget):
 
         return tags
 
-class TabBtn(QPushButton):
+    # 获取新标签
+    def getNewtag(self):
+        
+        tags = []  #获取已有标签列表
+        self.btn_tags = self.widget.findChildren(QPushButton)
+
+        # 获取新添加的标签
+        tags_add = self.lineEdit.text().split(",")
+        tags_add = []
+
+        for tag in tags_add:
+            if tag not in tags and tag != "": #如果新添加的标签已经选中或者空标签，则不添加到标签列表
+                tags_add.append(tag)
+                
+        return tags_add
+        
+
+
+class TagBtn(QPushButton):
     """标签按钮类"""
     def __init__(self,text):
-        super(TabBtn, self).__init__()
+        super(TagBtn, self).__init__()
         
         self.setText(text)  
         self.tagName = text #标签名字
